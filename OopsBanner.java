2e4254acd2627cd55a4b222175013b5cd032f65c
yokeshk09 <g.m.k.yokesh@gmail.com>
2026-03-02 13:25:11 +0530

    first

--- OopsBanner.java
+++ OopsBanner.java
@@ -1,26 +1,51 @@
 public class OopsBanner {
     public static void main(String[] args) {
-        String O1 = " ***** ";
-        String O2 = "*     *";
-        String O3 = "*     *";
-        String O4 = "*     *";
-        String O5 = " ***** ";
 
-        String P1 = "****** ";
-        String P2 = "*     *";
-        String P3 = "****** ";
-        String P4 = "*      ";
-        String P5 = "*      ";
-        String[] banner = {
-                String.join("   ", O1, O1, P1, P1),
-                String.join("   ", O2, O2, P2, P2),
-                String.join("   ", O3, O3, P3, P3),
-                String.join("   ", O4, O4, P4, P4),
-                String.join("   ", O5, O5, P5, P5)
-        };
+        String[] O = buildO();
+        String[] P = buildP();
+        String[] S = buildS();
+
+        String[] banner = new String[5];
+
+        for (int i = 0; i < 5; i++) {
+            banner[i] = String.join("   ", O[i], O[i], P[i], S[i]);
+        }
 
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    // Static method to build letter O
+    public static String[] buildO() {
+        return new String[] {
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    // Static method to build letter P
+    public static String[] buildP() {
+        return new String[] {
+                "****** ",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    // Static method to build letter S
+    public static String[] buildS() {
+        return new String[] {
+                " ***** ",
+                "*      ",
+                " ***** ",
+                "      *",
+                " ***** "
+        };
     }
 }
